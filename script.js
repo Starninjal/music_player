27d668df5af6f0a5c2a3bfca756a1dff25b0b777
Starninjal <BR17johnn@gmail.com>
2023-07-11 18:33:43 -0300

    update and finishing

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-function onChange(iconID){
-    if(document.getElementById(iconID).className=="fas fa-play-circle botao-play"){
-      document.getElementById(iconID).className = "fas fa-pause-circle";
-      document.getElementById(iconID).style.fontSize = 50
-    }else{
-      document.getElementById(iconID).className = "fas fa-play-circle botao-play";
-    }
-  }
+var rotationAngle = 0; // Variável para armazenar a posição de rotação atual
 
-  function onChangeArrow(iconID) {
-    document.getElementById(iconID).classList.add('changeArrowForward');
+function onChange(iconID) {
+  var playButton = document.getElementById(iconID);
+
+  if (playButton.className === "fas fa-play-circle botao-play") {
+    playButton.className = "fas fa-pause-circle";
+    var classAtual = document.getElementById(iconID).className.toString();
+
+    console.log(classAtual)
+    playButton.style.fontSize = "50px";
+    document.getElementById("img").classList.add("rotating");
+  } else {
+    playButton.className = "fas fa-play-circle botao-play";
+    document.getElementById("img").classList.remove("rotating");
+    var classAtual = document.getElementById(iconID).className.toString();
+    console.log(classAtual)
+
+    // Armazena a posição atual de rotação em graus
+    var imgStyle = window.getComputedStyle(document.getElementById("img"));
+    var transformValue = imgStyle.getPropertyValue("transform");
+    var values = transformValue.split("(")[1].split(")")[0].split(",");
+    var a = values[0];
+    var b = values[1];
+    rotationAngle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
   }
+}
+
+function onChangeArrow(iconID) {
+  document.getElementById(iconID).classList.add("changeArrowForward");
+}
+
+let musicas = [
+  {titulo: 'Lose Yourself', artista: 'Eminem', src:'Musicas/Love', img:'images/eminem.jpg               '}
+]
+
